80e07d3289fb32c115b49c5f6ee50d240efe2803
Andrea <andreamiralles686@gmail.com>
2017-08-07 15:58:35 -0700

    prompt player and update board with x or o

--- tictac.js
+++ tictac.js
@@ -1,30 +1,22 @@
-// var makeBoard = function() {
-//   var board = [];
-//   for (var j = 0; j < 3; j++) {
-//     var row = [];
-//     for (var i = 0; i < 3; i++) {
-//       row.push('*');
-//     }
-//     board.push(row);
-
-//   }
-
-//   return board;
-// };
-// console.log(makeBoard());
-
+var prompt = require('prompt');
 // [
 //   [ , , ], 
 //   [ , , ], 
 //   [ , , ]
 // ];
 
-var Player = function() {
-
+prompt.start();
+var onErr = function(err) {
+  console.log(err);
+  return 1;
 };
 
 var Game = function() {
   this.board = this.makeBoard();
+  // this.players = [1, 2];
+  // this.sign = ['x', 'o'];
+  this.players = {1: 'x', 2: 'o'};
+  this.currentPlayer = 2;
 };
 
 Game.prototype.makeBoard = function() {
@@ -41,6 +33,35 @@ Game.prototype.makeBoard = function() {
   return board;
 };
 
+// initialize
 var game = new Game();
-console.log(game.board);
+
+Game.prototype.turn = function() {
+  prompt.get(['row', 'cell'], (err, result) => {
+    if (err) { return onErr(err); }
+    console.log('row: ', result.row);
+    console.log('cell: ', result.cell);
+    if (this.currentPlayer === 1) {
+      this.board[result.row][result.cell] = this.players[1];
+      console.log(this.board); // has to go inside prompt.get()
+      this.currentPlayer = 2;
+    } else {
+      this.board[result.row][result.cell] = this.players[2];
+      console.log(this.board); // has to go inside prompt.get()
+      this.currentPlayer = 1;
+    }
+  });
+};
+
+// p1 turn
+game.turn();
+// game.turn(); // weird repeat input if do two in a row
+
+
+// console.log(game.board);
+
+// Game.prototype.winner = function() 
+
+
+
 
